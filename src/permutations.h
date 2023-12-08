9fea0f0026e0275218bac66aaa38ad8fe13fe9d8
BloatedMonke <pjlkkia@gmail.com>
2023-12-08 05:17:31 +0000

    slight changes to type qulifiers and names

--- src/permutations.h
+++ src/permutations.h
@@ -3,12 +3,17 @@
 
 #include <stdint.h>
 
+#define   perm_group(P) ((P).group)
+#define   perm_width(P) ((P).width)
+#define  perm_height(P) ((P).height)
+#define perm_objsize(P) ((P).objsize)
+
 /* a more fitting name for the operation */
 typedef uint8_t byte;
 
 struct perm
 {
-    void *collection;
+    void *group;
     uint64_t height;
     uint8_t width;
     uint32_t objsize;
@@ -34,17 +39,16 @@ perm;
  * [1,2,3] would be shown rather over [3,2,1] whereas
  * for combinations([3,4,2,1], 4, 3) [3,2,1] would be shown.
  */
-<<<<<<< HEAD:include/permutations.h
-perm nCkperm(void *collection, uint8_t n, uint8_t k, uint64_t objsize);
-=======
 perm combinations(void *collection, uint8_t n, uint8_t k, uint64_t objsize);
->>>>>>> main:src/permutations.h
 
 /**/
 void *cycle(void *collection, uint64_t n, uint64_t k, uint64_t objsize);
 
 /**/
 perm permutations(void *collection, uint64_t n, uint64_t k, uint64_t objsize);
+
+/**/
+void perm_kill(perm);
 
 /**/
 void printPerm(perm group, void (*prettyPrint)(void *));
