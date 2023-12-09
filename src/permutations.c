d3cba9f56b51ac24f4ca0cf9a6a1ff115eeaf373
BloatedMonke <pjlkkia@gmail.com>
2023-12-09 19:57:14 +0000

    make all struct members except the void* const + refactoring
    make const so they are not changeable by the user
    
    refactoring::
    - added new docstyle
    - use __restrict over restrict
    - added get macros for perm pointer

--- src/permutations.c
+++ src/permutations.c
@@ -3,6 +3,11 @@
 #include "combinatorics.h"
 #include "permutations.h"
 
+#define   get_perm_group(P) ((P)->group)
+#define   get_perm_width(P) ((P)->width)
+#define  get_perm_height(P) ((P)->height)
+#define get_perm_objsize(P) ((P)->objsize)
+
 typedef uint8_t byte;
 typedef unsigned int uint;
 typedef struct perm perm;
@@ -14,17 +19,19 @@ void perm_init(size_t objsize)
     PERM_OBJ_SIZE = objsize;
 }
 
-void perm_kill(perm A)
+void perm_kill(perm *A)
 {
-    free(perm_group(A));
+    free(get_perm_group(A));
 }
 
-/* automating for loops with recursion */
-void enumerate(int * restrict rowN, int ijk, int k, int counters[k], int ijk_ends[k], byte * restrict arr, byte * restrict group)
+/*-------------------------------------
+ * automating for loops with recursion
+ *-----------------------------------*/
+void rearrange(int * __restrict rowN, int ijk, int k, int counters[k], int ijk_ends[k], byte * __restrict arr, byte * __restrict group)
 {
     if (ijk == k) {
         for (int iter = 0; iter < k; ++iter)
-            memcpy(group + (*rowN * k + iter) * PERM_OBJ_SIZE, collection + counters[iter] * PERM_OBJ_SIZE, PERM_OBJ_SIZE);
+            memcpy(group + (*rowN * k + iter) * PERM_OBJ_SIZE, arr + counters[iter] * PERM_OBJ_SIZE, PERM_OBJ_SIZE);
 
         ++*rowN;
         return;
@@ -35,7 +42,7 @@ void enumerate(int * restrict rowN, int ijk, int k, int counters[k], int ijk_end
          counters[ijk] < ijk_ends[ijk];
          ++counters[ijk])
 
-        enumerate(rowN, ijk + 1, k, counters, ijk_ends, arr, group);
+        rearrange(rowN, ijk + 1, k, counters, ijk_ends, arr, group);
 
     return;
 }
@@ -54,7 +61,7 @@ perm combinations(void *collection, uint8_t n, uint8_t k)
         ijk_ends[i] = n - k + i + 1;
     }
 
-    enumerate(&rowN, 0, k, counters, ijk_ends, collection, group);
+    rearrange(&rowN, 0, k, counters, ijk_ends, collection, group);
 
     return (perm){.group = group, .width = k, .height = height, .objsize = PERM_OBJ_SIZE};
 }
@@ -67,7 +74,7 @@ void base_perm_swap(uint A[], int i, int j)
 }
 
 /* TODO */
-void cycle(byte * restrict collection, byte * restrict group, uint64_t n, uint64_t k, uint base_perm[k])
+void cycle(byte* __restrict collection, byte* __restrict group, uint8_t n, uint8_t k, uint base_perm[k])
 {
     /* PHONY: */
     n += k;
@@ -77,7 +84,7 @@ void cycle(byte * restrict collection, byte * restrict group, uint64_t n, uint64
     /* END PHONY */
 }
 
-perm permutations(void *collection, uint64_t n, uint64_t k)
+perm permutations(void *collection, uint8_t n, uint8_t k)
 {
     uint64_t height = nPr(n, k);
 
@@ -100,16 +107,16 @@ perm permutations(void *collection, uint64_t n, uint64_t k)
 
 #include <stdio.h>
 
-void print_perm(perm group, void (*pretty_print)(void *))
+void print_perm(perm *group, void (*pretty_print)(void *))
 {
     printf("[\n");
-    for (uint i = 0; i < group.height; ++i){
+    for (uint i = 0; i < get_perm_height(group); ++i){
         printf("[");
-        for (uint j = 0; j < group.width; ++j){
-            pretty_print( (void *)&((byte *)group.group)[ (i * group.width + j) * group.objsize] );
-            printf("%s", j < group.width - 1u ? ", ": "");
+        for (uint j = 0; j < get_perm_width(group); ++j){
+            pretty_print( (void *)&((byte *)get_perm_group(group))[ (i * get_perm_width(group) + j) * get_perm_objsize(group)] );
+            printf("%s", j < get_perm_width(group) - 1u ? ", ": "");
         }
-        printf("]%s\n", i < group.height - 1 ? ",": "");
+        printf("]%s\n", i < get_perm_height(group) - 1 ? ",": "");
     }
     printf("]\n");
 }
--- src/permutations.h
+++ src/permutations.h
@@ -11,44 +11,53 @@
 
 struct perm
 {
-    void *group;
-    uint64_t height;
-    uint8_t width;
-    uint32_t objsize;
+    void*           group;
+    const uint64_t height;
+    const uint8_t   width;
+    const size_t  objsize;
 };
 
-/* perm_init is to be called before any perm takes place.
- * any perm henceforth will assume that objsize until 
- * perm_init is called again with a new objsize */
+/*------------------------------------------------------------------
+ *  perm_init is to be called before any perm takes place. Any perm
+ * henceforth will assume that objsize until perm_init is called
+ * again with a new objsize
+ *----------------------------------------------------------------*/
 void perm_init(size_t objsize);
 
-/* all of these functions will allocate memory for a new 
- * container of size (width * objsize) * (height) =
- * (k * objsize) * P(n, k) where P denotes the standard
- * permute function for each permutation listed.
- *
- * These must be freed by the caller.
- */
+/***
+ """
+     All of these functions will allocate memory for a new container
+    of size (width * objsize) * (height) = (k * objsize) * P(n, k)
+    where P denotes the standard combinatorics function for each
+    rearrangement function listed.
+     These must be freed by the caller using perm_kill(&P).
+ """
+ **/
 
-/* permutes the array such that both the elements
- * and the ordering are unique. i.e. only one of
- * either [1,2,3] or [3,2,1] will exist.
- *
- * The ordering takes first encountered elements
- * to be of higher priority. i.e. from the example
- * above if the call was combinations([1,2,3,4], 4, 3)
- * [1,2,3] would be shown rather over [3,2,1] whereas
- * for combinations([3,4,2,1], 4, 3) [3,2,1] would be shown.
- */
-perm combinations(void *collection, uint8_t n, uint8_t k);
+/*-------------------------------------------------------------------
+ *  Permutes the array such that both the elements and the ordering
+ * are unique. i.e. , if the call was combinations([1,2,3,4], 4, 3),
+ * only one of either [1,2,3] or [3,2,1] will exist.
+ *  The ordering takes first encountered elements to be of higher
+ * priority. i.e. from the example above [1,2,3] would be preferred
+ * over [3,2,1]. Whereas for combinations([3,4,2,1], 4, 3) [3,2,1]
+ * would be shown.
+ *-----------------------------------------------------------------*/
+struct perm combinations(void* collection, uint8_t n, uint8_t k);
 
-/**/
-perm permutations(void *collection, uint64_t n, uint64_t k);
+/*---------------------------------------------------------
+ * Returns all possible rearrangemenents of the collection
+ *-------------------------------------------------------*/
+struct perm permutations(void* collection, uint8_t n, uint8_t k);
 
-/* free the allocated 2D perm array */
-void perm_kill(perm);
+/*----------------------------------
+ * free the allocated 2D perm array
+ *--------------------------------*/
+void perm_kill(struct perm*);
 
-/* print the perms with a  */
-void print_perm(perm group, void (*pretty_print)(void *));
+/*-------------------------------------------------------
+ * print the perms with a pretty printer for the objects
+ *-----------------------------------------------------*/
+void print_perm(struct perm*, void (*pretty_print)(void *));
 
 #endif /* _PERMUTATIONS_H_ */
