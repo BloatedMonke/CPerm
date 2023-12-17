815ffe30fa9d716c6da11c221b66151f8178a49d
BloatedMonke <pjlkkia@gmail.com>
2023-12-17 08:44:59 +0000

    move combinatoric functions to the main source file
    Makes for a more simple build

--- src/cperm.c
+++ src/cperm.c
@@ -120,9 +120,39 @@ perm combinations(void *collection, uint8_t n, uint8_t k, size_t size)
     return (perm){.group = group, .width = k, .height = height, .objsize = size};
 }
 
-/* =====================================================
- * utility funcs
- ==================================================== */
+/* ========================
+ * Combinatorics
+ ========================*/
+
+uint64_t nCk(uint64_t n, uint64_t k)
+{
+    return nPr(n, k) / fact(k);
+}
+
+uint64_t nPr(uint64_t n, uint64_t r)
+{
+    if (r <= 0) return 1;
+    uint64_t p = 1;
+    for (uint8_t i = n - r + 1; i <= n; ++i) {
+        p *= i;
+    }
+    return p;    
+}
+
+uint64_t fact(uint64_t n)
+{
+    if (n == 0) return 1;
+    uint64_t p = 1;
+    for (uint8_t i = 2; i <= n; ++i) {
+        p *= i;
+    }
+    return p;
+}
+
+
+/* ========================
+ * Utility
+ ========================*/
 
 #include <stdio.h>
 
