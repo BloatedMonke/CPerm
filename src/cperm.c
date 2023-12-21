44c8b79b8bc7d29dd40687bfbc8bdb972550c000
BloatedMonke <pjlkkia@gmail.com>
2023-12-21 14:46:04 +0000

    Squashed commit of main:
    commit 08ca8f9e17bfdfefa34a7b2ca74725770ca709ea
    Author: BloatedMonke <88450430+BloatedMonke@users.noreply.github.com>
    Date:   Thu Dec 21 06:46:54 2023 +0000
    
        more test files; micro-optimisations and c89 transforms; fmt fmt fmt (#11)
    
        * change struct test file name to a much more descriptive one
    
        * add new testers
    
        * more test files; micro-optimisations and c89 transforms; fmt fmt fmt
    
        CPERM
        - ensure that permutations and combinations return an empty list in the
          correct circumstances
        - micro-optimisations in permutations
        - added new print functions: perm_print, perm_fprint
        - print_perms -> perm_print_all
    
        TESTS
        - only print statements when asserts are false
        - now use make test_clean to delete files after test
    
    commit e2bfe0ac99e913a1b767251169df366e3da77ab8
    Date:   Wed Dec 20 03:41:00 2023 +0000
    
        Ensure users know to kill the perm
    
    commit e9e3b33d5231ad8d462659de893a29c7225a50f9
    Date:   Wed Dec 20 03:37:50 2023 +0000
    
        revert readme to previous commit
    
    commit d696452ea4fff431a2682253cd2661a201449ae4
    Date:   Wed Dec 20 03:06:42 2023 +0000
    
        fix k < n not working (#10)
    
        the end for the rotation loop was k rather than n
    
    commit 3432fc6eb0a635c3e97a6446a9bd1c0a3af45b5b
    Date:   Tue Dec 19 20:50:22 2023 +0000
    
        Ensure users know to call perm_kill
    
        and also update example

--- src/cperm.c
+++ src/cperm.c
@@ -18,9 +18,17 @@
 #define   get_ph(P) ((P)->height)
 #define  get_pos(P) ((P)->objsize)
 
-typedef uint8_t byte;
+typedef unsigned char byte;
 typedef unsigned int uint;
 typedef struct perm perm;
+struct internal_perm {
+    void*     group;
+    uint64_t height;
+    uint8_t   width;
+    size_t  objsize;
+};
+typedef struct internal_perm
+INTERNAL_PERM_T;
 
 void perm_kill(perm *A)
 {
@@ -38,46 +46,59 @@ perm permutations(void *collection, uint8_t n, uint8_t k, size_t size)
 {
     uint64_t height = nPr(n, k);
 
-    uint seed[n], cycles[k], rowN = 0;
+    INTERNAL_PERM_T circle;
+    byte *group;
+    byte *pool;
+
+    bool no_break = false;
+    group = malloc(height * k * size);
+    pool = (byte *)collection;
+
+    if (0 == size || !group || k > n || 0 == k) {
+        free(group);
+        circle.group = NULL;
+        circle.width = 0;
+        circle.height = 1;
+        circle.objsize = 0;
+        n = k = 0;
+        no_break = true;
+    }
+    
+
+    uint seed[n], cycles[k], rowN;
     for (uint i = 0; i < n; ++i) seed[i] = i;
     for (uint i = 0; i < k; ++i) cycles[i] = n - i;
-
-    byte *group = malloc(height * k * size);
-    byte *pool = (byte *)collection;
-    for (uint i = 0; i < k; ++i)
-        memcpy(group + (rowN * k + i) * size, pool + (seed[i]) * size, size);
-    ++rowN;
+    for (uint i = 0; i < k; ++i) memcpy(group + i * size, pool + (seed[i]) * size, size);
     
-    bool no_break = false;
+    rowN = 1;
     while (true) {
         if (no_break) break;
         
         for (int i = k-1; i >= 0; --i) {
             cycles[i] -= 1;
             
-            if (cycles[i] == 0) {
-                uint tmp = seed[i];
-                
-                for (uint j = i + 1; j < k; ++j)
-                    seed[j-1] = seed[j];
-        
-                seed[k-1] = tmp;
-                cycles[i] = n - i;
-            }
-            
-            else {
+            if (cycles[i] != 0) {
                 base_perm_swap(seed, i, n - cycles[i]);
                 for (uint j = 0; j < k; ++j)
                     memcpy(group + (rowN * k + j) * size, pool + (seed[j]) * size, size);
-                
                 ++rowN;
                 no_break = false;
                 break;
-            }
-            no_break = true;
+            }            
+            for (uint j = i + 1; j < n; ++j)
+                base_perm_swap(seed, j-1, j);
+    
+            cycles[i] = n - i;
+            no_break = true;            
         }
     }
-    return (perm){.group = group, .width = k, .height = height, .objsize = size};
+    if (k <= n && 0 != k) {
+        circle.group = group;
+        circle.width = k;
+        circle.height = height;
+        circle.objsize = size;
+    }
+    return *(struct perm *)&circle;
 }
 
 /*-------------------------------------
@@ -111,7 +132,7 @@ perm combinations(void *collection, uint8_t n, uint8_t k, size_t size)
     
     byte *group = malloc(height * k * size);
 
-    if (size < 1 || !group) {
+    if (0 == size || !group || k > n || 0 == k) {
         height = k = 0;
         free(group);
         group = NULL;
@@ -165,7 +186,7 @@ uint64_t fact(uint64_t n)
 
 #include <stdio.h>
 
-void print_perm(perm *group, void (*pretty_print)(void *))
+void perm_print_all(perm *group, void (*pretty_print)(void *))
 {
     printf("[\n");
     for (uint i = 0; i < get_ph(group); ++i) {
@@ -177,4 +198,26 @@ void print_perm(perm *group, void (*pretty_print)(void *))
         printf("]%s\n", i < get_ph(group) - 1 ? ",": "");
     }
     printf("]\n");
+}
+
+void perm_print(perm *group, size_t idx, void (*pretty_print)(void *))
+{
+    if (idx > get_ph(group)) {fprintf(stderr, "ERROR:: index out of rangr\n" ); exit(0xf9);}
+    printf("[");
+    for (uint j = 0; j < get_pw(group); ++j) {
+        pretty_print( (void *)&((byte *)get_pg(group))[ (idx * get_pw(group) + j) * get_pos(group)] );
+        printf("%s", j < get_pw(group) - 1u ? ", ": "");
+    }
+    printf("]\n");
+}
+
+void perm_fprint(FILE *file, perm *group, size_t idx, void (*pretty_fprint)(FILE *, void *))
+{
+    if (idx > get_ph(group)) {fprintf(stderr, "ERROR:: index out of rangr\n" ); exit(0xf9);}
+    fprintf(file, "[");
+    for (uint j = 0; j < get_pw(group); ++j) {
+        pretty_fprint(file, (void *)&((byte *)get_pg(group))[ (idx * get_pw(group) + j) * get_pos(group)] );
+        fprintf(file, "%s", j < get_pw(group) - 1u ? ", ": "");
+    }
+    fprintf(file, "]\n");
 }
